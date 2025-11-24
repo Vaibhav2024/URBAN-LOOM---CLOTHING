00e6ca9000d55926dbf871153f6b0a5d7e5b3c39
Vaibhav2024 <vaibhavpatil090604@gmail.com>
2025-11-24 12:12:36 +0530

    Integrated Razorpay and Stripe payment gateway successfully

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,4 +1,4 @@
-import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus} from '../controllers/orderController.js'
+import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay} from '../controllers/orderController.js'
 import adminAuth from '../middleware/adminAuth.js'
 import express from 'express'
 import authUser from '../middleware/auth.js'
@@ -16,5 +16,10 @@ orderRouter.post('/razorpay', authUser, placeOrderRazorpay)
 
 // User feature
 orderRouter.post('/userorders', authUser, userOrders)
+
+
+//verify payment
+orderRouter.post('/verifyStripe', authUser, verifyStripe)
+orderRouter.post('/verifyRazorpay', authUser, verifyRazorpay)
 
 export default orderRouter
